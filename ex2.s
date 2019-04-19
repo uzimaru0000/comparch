fc3ec789814fa99484a55c552a9a650e8f6658c3
s1250087-oba <s1250087@u-aizu.ac.jp>
2019-04-19 11:40:43 +0900

    ex2

---
+++ ex2.s
@@ -0,0 +1,34 @@
+    .data
+A:  .word 13
+B:  .word 37
+C:  .word 0
+
+    .text
+main:
+    lw $8, A # load A
+    lw $9, B # load B
+    addi $10, $0, 1 # mask
+    addi $11, $0, 0 # i
+    addi $12, $0, 16 # N
+    addi $14, $0, 0 # ans
+
+loop:
+    beq $11, $12, loopend
+    and $24, $8, $10
+    beq $24, $0, inc
+    addu $14, $14, $9
+    j inc
+
+inc:
+    addi $11, $11, 1
+    addu $10, $10, $10
+    addu $9, $9, $9
+    j loop
+
+loopend:
+    sw $14, C
+    
+exit:
+    j exit
+
+
